4c1521dd56e112274372174f83f7cf9c8c62ea89
cryju <cryju@DESKTOP-9EBU2QH>
2022-07-21 22:56:57 +0900

    초기 커밋.

---
+++ A Math Problem in Elementary School/A Math Problem in Elementary School.c
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	int num1, num2 = 0;
+	int answer = 0;
+	printf("Automatically generate arithmetic problems.\n");
+
+	while (1) {
+		srand((int)time(NULL));
+		num1 = rand() % 100;
+		num2 = rand() % 100;
+		printf("%d + %d = ", num1, num2);
+		scanf("%d", &answer);
+		if (answer != num1 + num2) {
+			printf("Wrong Answer.\n");
+			continue;
+		}
+		else if (answer = num1 + num2) {
+			printf("Great Job.\n");
+			break;
+		}
+		else {
+			printf("Wrong Answer.\n");
+			continue;
+		}
+	}
+
+	return 0;
+}
